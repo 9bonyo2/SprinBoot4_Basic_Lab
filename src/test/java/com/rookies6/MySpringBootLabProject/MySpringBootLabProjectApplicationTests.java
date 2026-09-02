c0d37cc76209ad586313031c8195f8709abb6a77
9bonyo2 <2byku999999999@gmail.com>
2026-09-02 23:11:48 +0900

    first commit

---
+++ src/test/java/com/rookies6/MySpringBootLabProject/MySpringBootLabProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookies6.MySpringBootLabProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootLabProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
