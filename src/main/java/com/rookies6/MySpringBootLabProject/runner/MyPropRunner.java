5e5f259ec2f1dbe48c1ce2e2b4c3499dd9570d1c
9bonyo2 <2byku999999999@gmail.com>
2026-09-04 00:22:44 +0900

    260903제ì출 실습1-5 MyProp습 ãMyPrties1-5 MyPropPerties 클래스 작성

--- src/main/java/com/rookies6/MySpringBootLabProject/runner/MyPropRunner.java
+++ src/main/java/com/rookies6/MySpringBootLabProject/runner/MyPropRunner.java
@@ -1,5 +1,6 @@
 package com.rookies6.MySpringBootLabProject.runner;
 
+import com.rookies6.MySpringBootLabProject.property.MyPropProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,13 +23,19 @@ public class MyPropRunner implements ApplicationRunner {
     @Value("${myprop.port}")
     private int port;
 
+    @Autowired
+    private MyPropProperties properties;
+
     private Logger logger = LoggerFactory.getLogger(MyPropRunner.class);
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        logger.info("Logger 구현체 클래스명 {}", logger.getClass().getName());
-        logger.info("MyRunner run() 호출됨!!");
-        logger.info("UserName = {}", username);
-        logger.info("port = {}", port);
+//        logger.info("Logger 구현체 클래스명 {}", logger.getClass().getName());
+//        logger.info("MyRunner run() 호출됨!!");
+//        logger.info("UserName = {}", username);
+//        logger.info("port = {}", port);
+
+        logger.info("MyPropProperties getUsername() = {}", properties.getUsername());
+        logger.info("MyPropProperties getPort() = {}", properties.getPort());
     }
 }
